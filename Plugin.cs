39cb041614ff37f336f85cb5fecae4afe11e1498
musigtt <139933103+musigtt@users.noreply.github.com>
2023-10-20 14:29:18 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace NoEyes
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        void Start()
+        {
+            /* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/Halloween2023TreeRoom/Halloween2023TreeRoom_Gameplay").SetActive(false);
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace NoEyes
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.musigt.gorillatag.noeyes";
+        public const string Name = "NoEyes";
+        public const string Version = "1.0.0";
+    }
+}
